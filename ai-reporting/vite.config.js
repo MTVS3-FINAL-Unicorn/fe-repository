3175f7f390b942261e51944ac912db57d19327ff
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2024-11-13 17:05:21 +0900

    [Settings] port 수정 및 proxy 설정 추가

--- ai-reporting/vite.config.js
+++ ai-reporting/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: 904,
+    host: '0.0.0.0',
+      proxy: {
+        '/api': {
+          target: 'http://localhost',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '/api/v1/report'),
+        },
+      },
+  },
   plugins: [react()],
 })
